d6c9a5a6b024ecd4ad10dc3bf31a03cb1b348a69
Thuc Trinh <trinhhongthuc57@gmail.com>
2023-09-13 16:31:12 +0700

    code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Build code Reactjs with jenkins local</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Build Reactjs Web</h1>
     </>
   )
 }
