89d0663c0d287a903f70daa627fc738403d9684f
Thuc Trinh <65403020+trinhhongthuc@users.noreply.github.com>
2023-09-13 15:51:08 +0700

    Change title app (#4)
    * Change content jenkin file
    
    * Change content jenkin file
    
    * Change title app
    
    * Remove code

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Build code Reactjs with jenkins local</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
