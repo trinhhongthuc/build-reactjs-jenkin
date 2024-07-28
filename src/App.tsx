cb3c60a3d1d59da9227acd70f5bda59a2e14ca15
Thuc Trinh <trinhhongthuc57@gmail.com>
2024-07-28 14:05:15 +0700

    Change file app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
   return (
     <>
-      <h1>
-        Build Reactjs Web</h1>
+      <h1>Build Reactjs Web</h1>
+
+      <p>This is a simple ReactJS app.</p>
+      <p>I building successfully about jenkins with docker, docker-compose</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
